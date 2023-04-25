edc6932f62d71da3c356e36f1048ba49d2ee0d2a
kavinsingh1502 <75671178+kavinsingh1502@users.noreply.github.com>
2023-04-25 22:07:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+song1="";
+song2="";
+function preload(){
+    song1=loadSound("gangam.mp3");
+    song2=loadSound("jhoome.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,500);
+}
